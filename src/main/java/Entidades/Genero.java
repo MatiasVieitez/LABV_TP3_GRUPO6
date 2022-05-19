b135a338579c509e01054f6af969f910e1517796
Ezequiel Fernando Aguirre <ezefer96@gmail.com>
2022-05-18 21:42:38 -0300

    Agregado Genero.java

---
+++ src/main/java/Entidades/Genero.java
@@ -0,0 +1,50 @@
+package Entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Genero")
+public class Genero implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name = "ID")
+	private Integer ID;
+	@Column(name = "Descripcion")
+	private String Descripcion;
+
+	public Genero() {
+	}
+
+	// Gets y sets
+	public Integer getID() {
+		return ID;
+	}
+
+	public void setID(Integer iD) {
+		ID = iD;
+	}
+
+	public String getDescripcion() {
+		return Descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+
+	public void setGenero(String genero) {
+		Descripcion = genero;
+	}
+
+	@Override
+	public String toString() {
+		return "Genero [id=" + ID + ", Genero=" + Descripcion + "]";
+	}
+
+}
